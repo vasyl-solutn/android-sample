ae64790c6a4cc2a9313846ae252d7545c13d89b9
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-06-02 01:35:12 +0300

    the part two

--- app/src/main/java/com/example/myapp1434/MainActivity.kt
+++ app/src/main/java/com/example/myapp1434/MainActivity.kt
@@ -1,7 +1,9 @@
+// File: app/src/main/java/com/example/myapp1434/ui/MainActivity.kt
 package com.example.myapp1434
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -16,20 +18,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
+        val textViewName: TextView = findViewById(R.id.textViewName)
+        val textViewEmail: TextView = findViewById(R.id.textViewEmail)
         val button: Button = findViewById(R.id.button)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
 
-        // Using lambdas for LiveData observers
         viewModel.user.observe(this) { user ->
-            textView.text = user.name
+            textViewName.text = "Name: ${user.name}"
+            textViewEmail.text = "Email: ${user.email}"
+            progressBar.visibility = ProgressBar.GONE
         }
 
         viewModel.error.observe(this) { error ->
             Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+            progressBar.visibility = ProgressBar.GONE
+        }
+
+        viewModel.loading.observe(this) { isLoading ->
+            if (isLoading) {
+                progressBar.visibility = ProgressBar.VISIBLE
+            } else {
+                progressBar.visibility = ProgressBar.GONE
+            }
         }
 
         button.setOnClickListener {
-            viewModel.fetchUser(6) // Fetch user with ID 1
+            viewModel.fetchUser(7) // Fetch user with ID 1
         }
     }
 }
--- app/src/main/java/com/example/myapp1434/ui/UserViewModel.kt
+++ app/src/main/java/com/example/myapp1434/ui/UserViewModel.kt
@@ -1,3 +1,4 @@
+// File: app/src/main/java/com/example/myretro/ui/UserViewModel.kt
 package com.example.myapp1434.ui
 
 import androidx.lifecycle.*
@@ -12,13 +13,19 @@ class UserViewModel : ViewModel() {
     private val _error = MutableLiveData<String>()
     val error: LiveData<String> get() = _error
 
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> get() = _loading
+
     fun fetchUser(userId: Int) {
+        _loading.value = true
         viewModelScope.launch {
             try {
                 val fetchedUser = RetrofitClient.apiService.getUser(userId)
                 _user.postValue(fetchedUser)
             } catch (e: Exception) {
                 _error.postValue("Failed to fetch user")
+            } finally {
+                _loading.postValue(false)
             }
         }
     }
